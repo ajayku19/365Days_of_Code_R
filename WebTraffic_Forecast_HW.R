2fa0deabfc2a8a76d9752555ea085de920985b85
AjayK <50428623+ajayku19@users.noreply.github.com>
2021-01-03 18:06:08 -0700

    Add files via upload

---
+++ WebTraffic_Forecast_HW.R
@@ -0,0 +1,27 @@
+library(googleAnalyticsR)
+library(googleAuthR)
+
+viewid<-149406111
+start_date<-"2017-05-01"
+end_date<-"2020-12-30"
+
+df2 <- google_analytics_4(viewid, date_range = c(start_date, end_date),metrics = c("sessions"),dimensions = c("yearMonth"))
+df2 <- google_analytics_4(viewid, date_range = c(start_date, end_date),metrics = c("sessions"),dimensions = c("yearMonth", "channelgrouping"))   #filter organic traffic
+
+write.csv(df2, file="C:/Users/ZBFZHR/Desktop/Tidytuesday/datasets/sessions.csv", row.names=FALSE)  
+#filter organic and import
+
+sessions <- read.csv("C:/Users/ZBFZHR/Desktop/Tidytuesday/datasets/sessions.csv")
+df<-sessions
+
+head(df, n=10)
+ga_ts <- ts(df$sessions, start = c(2017,05), end = c(2021,01), frequency = 12)
+
+plot(ga_ts)
+library(TSA)
+library(tseries)
+library(forecast)
+kpss.test(ga_ts)
+
+fcast<-hw(ga_ts, seasonal="additive")
+plot(fcast)
